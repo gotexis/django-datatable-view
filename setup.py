baff8a8372b15e9518ea8376f389f0ed09402d84
Exis Zhang <38595828+gotexis@users.noreply.github.com>
2019-03-04 11:33:46 +0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,28 +1,17 @@
 # -*- encoding: utf-8 -*-
-# wtf
 """setup.py: Django django-datatable-view-compat"""
 
 from setuptools import setup, find_packages
 
-try:
-    import pypandoc
-    long_description = pypandoc.convert('README.md', 'rst')
-except (IOError, ImportError, OSError):
-    long_description = open('README.md').read()
 
 setup(
     name='django-datatable-view-compat',
     version='0.8.3',
-    description='This package is used in conjunction with the jQuery plugin '
-                '(http://http://datatables.net/), and supports state-saving detection'
-                ' with (http://datatables.net/plug-ins/api).  The package consists of '
-                'a class-based view, and a small collection of utilities for rendering'
-                ' table data from models.',
-    long_description=long_description,
-    author='utapyngo',
-    author_email='ut@pyngo.tom.ru',
-    url='https://github.com/utapyngo/django-datatable-view',
-    download_url='https://github.com/utapyngo/django-datatable-view/tarball/django20',
+    description='fucking version',
+    author='exis',
+    author_email='exis@qq.com',
+    url='https://github.com/gotexis/django-datatable-view',
+    download_url='https://github.com/gotexis/django-datatable-view/tarball/compat083',
     license='Apache License (2.0)',
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
@@ -35,8 +24,7 @@ setup(
         'Topic :: Software Development',
     ],
     packages=find_packages(exclude=['tests', 'tests.*']),
-    package_data={'datatableview': ['static/js/*.js', 'templates/datatableview/*.html']},
+    package_data={'datatableview_compat': ['static/js/*.js', 'templates/datatableview_compat/*.html']},
     include_package_data=True,
-    setup_requires=['pypandoc==1.4'],
     install_requires=['django>=2.0', 'python-dateutil>=2.1'],
 )
